721157acffd500a26d81e95794a0cab39be3abe3
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-28 22:51:22 +0900

    データベースのテーブルにあたるEntityを定義

---
+++ TestLocalDatabase/app/src/main/java/com/example/testlocaldatabase/Todo.kt
@@ -0,0 +1,13 @@
+package com.example.testlocaldatabase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+class Todo (
+    @PrimaryKey(autoGenerate = true)
+        val id:Int,
+        val title:String = "",
+        val created_at: Date = Date()
+)
