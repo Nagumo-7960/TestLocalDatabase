aef683ba4aec9bcd2c902c14362ab07aab042add
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-28 23:08:58 +0900

    DataTimeConverter, RoomDatabase, RoomApplicationクラスを作成

---
+++ TestLocalDatabase/app/src/main/java/com/example/testlocaldatabase/DateTimeConverter.kt
@@ -0,0 +1,16 @@
+package com.example.testlocaldatabase
+
+import androidx.room.TypeConverter
+import java.sql.Date
+
+class DateTimeConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time?.toLong()
+    }
+}
